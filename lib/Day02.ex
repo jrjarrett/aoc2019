4b613c6703c1a6860931c8703d331446cbf6543e
Jim Jarrett <jarrett@lypsinc.org>
2019-12-04 22:36:43 -0600

    Day 2 Part 1 completed.

--- lib/Day02.ex
+++ lib/Day02.ex
@@ -4,23 +4,45 @@ defmodule Day02 do
   """
   @external_resource "priv/day-2.txt"
   @input File.read!("priv/day-2.txt")
-     |> String.trim
-     |> String.split(",", trim: true)
-     |> Enum.map(&String.to_integer/1)
+         |> String.trim()
+         |> String.split(",", trim: true)
+         |> Enum.map(&String.to_integer/1)
 
+  @instructionSize 4
 
-  def foo(input) do
-    counter = 0
-    [pc | memory] = Enum.chunk_every(Enum.drop(input,counter*4),4)
-    [opcode | rest] = pc
-    [op1 | rest] = rest
+  def runProgram(input, programCounter) do
+    [instruction | _memory] =
+      Enum.chunk_every(Enum.drop(input, programCounter * @instructionSize), @instructionSize)
+
+    [opcode | operation] = instruction
+
+    case opcode do
+      opcode when opcode != 99 ->
+        runProgram(performOperation(input, opcode, operation), programCounter + 1)
+
+      opcode when opcode == 99 ->
+        input
+    end
+  end
+
+  def performOperation(memory, opcode, operation) do
+    [op1 | rest] = operation
     [op2 | rest] = rest
-    [memloc | rest] = rest
-    List.replace_at(input, memloc, Enum.at(input,op1) + Enum.at(input, op2))
-  end
-  
-  def foo1 do 
-    foo(@input)
+    [memloc | _rest] = rest
+    List.replace_at(memory, memloc, operation(opcode, Enum.at(memory, op1), Enum.at(memory, op2)))
   end
 
+  def operation(opcode, operand1, operand2) when opcode == 1 do
+    operand1 + operand2
+  end
+
+  def operation(opcode, operand1, operand2) when opcode == 2 do
+    operand1 * operand2
+  end
+
+  def runDay2 do
+    gravAssist = List.replace_at(@input, 1, 12)
+    gravAssist = List.replace_at(gravAssist, 2, 2)
+    runProgram(gravAssist, 0)
+  end
 end
---
+++ test/day02_test.exs
@@ -0,0 +1,58 @@
+defmodule Day02Test do
+  use ExUnit.Case
+  doctest Day02
+
+  test "addition works" do
+    assert Day02.operation(1, 5, 4) == 9
+  end
+
+  test "multiplication works" do
+    assert Day02.operation(2, 5, 4) == 20
+  end
+
+  # test "foo works?" do
+  #   input = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+  #   assert Day02.foo(input) == [1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+  # end
+
+  test "performOperation works" do
+    input = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+    expected = [1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+
+    assert Day02.performOperation(input, 1, [9, 10, 3]) == expected
+  end
+
+  test "test program 1" do
+    input = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+    expected = [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+    assert Day02.runProgram(input, 0) == expected
+  end
+
+  test "test program 2" do
+    input = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+    expected = [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+    assert Day02.runProgram(input, 0) == expected
+  end
+
+  test "test program 3" do
+    input = [1, 0, 0, 0, 99]
+    expected = [2, 0, 0, 0, 99]
+    assert Day02.runProgram(input, 0) == expected
+  end
+
+  test "test program 4" do
+    input = [2, 3, 0, 3, 99]
+    expected = [2, 3, 0, 6, 99]
+    assert Day02.runProgram(input, 0) == expected
+  end
+
+  test "test program 5" do
+    input = [1, 1, 1, 4, 99, 5, 6, 0, 99]
+    expected = [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    assert Day02.runProgram(input, 0) == expected
+  end
+
+  test "Day 2 Part 1" do
+    assert Enum.at(Day02.runDay2(), 0) == 4_484_226
+  end
+end
